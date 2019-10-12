271bff46d7548f86942ad9d89663b16067a47486
mjedwards <jairedwards20@gmail.com>
2019-10-11 22:00:16 -0400

    finished majority

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,79 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+let container = document.querySelector('.cards-container');
+let empty = '#';
+let name = "John Doe";
+let fakeHeadLine = "Oranges are great";
+let authorName;
+let authorPhoto;
+let newsHeadline;
+
+
+  axios.get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(function (response) {
+    // handle success
+
+    console.log(response);
+    createCard(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+
+function createCard (x) {
+    let newsArticles= [];
+    let newsBootstrap = Object.values(x.data.articles.bootstrap[i]);
+    let newsJavaScript= Object.values(x.data.articles.javascript[i]);
+    let newsJquery = Object.values(x.data.articles.jquery[i]);
+    let newsNode = Object.values(x.data.articles.node[i]);
+    let newsTech = Object.values(x.data.articles.technology[i]);
+
+    for(let i = 0; i < 5; i++) {
+        newsBootstrap;
+        newsJavaScript;
+        newsJquery;
+        newsNode;
+        newsTech; 
+    }
+     
+   
+
+  let card = document.createElement("div");
+    card.classList.add("card");
+
+    let headLine = document.createElement("div");
+    headLine.classList.add("headline");
+    headLine.textContent = `${newsHeadline}`;
+
+    
+    let author = document.createElement("div");
+    author.classList.add("author");
+
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("img-container");
+
+  let image = document.createElement("img");
+    image.setAttribute("src",  `${authorPhoto}`);
+
+  let spaN = document.createElement("span");
+    spaN.textContent = `By ${authorName}`;
+    
+
+// card assembly
+  container.appendChild(card);
+    card.appendChild(headLine);
+    card.appendChild(author);
+        author.appendChild(imgContainer);
+            imgContainer.appendChild(image);
+        author.appendChild(spaN);
+
+  return container;
+}
+
+createCard();
